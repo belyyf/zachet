ddf507bbf5cad9ea75f63b39c03d6ac1a43a50a8
daniil <belyjegor45@gmail.com>
2026-04-17 13:40:33 +0300

    еще исправление

--- app/database.py
+++ app/database.py
@@ -3,10 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import config
 
-SQLALCHEMY_DATABASE_URL = config.DATABASE_URL.replace("aiosqlite:///", "sqlite:///")
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
